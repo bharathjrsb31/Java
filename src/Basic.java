2f6382248be979939c0d14d78571c7e16c10f9ac
Bharathjrsb31 <Bharath.j@dhlsdigital.com>
2022-09-06 19:46:19 +0530

    first

---
+++ src/Basic.java
@@ -0,0 +1,24 @@
+public class Basic {
+    public static void main(String[] args) {
+        int myNumber = 88;
+        short myShort = 850;
+        long myLong = 9570;
+
+        double myDouble = 7.5554;
+        float myFloat = 565.5f;
+
+        char myChar = 'y';
+        boolean myBoolean = false;
+
+        byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
